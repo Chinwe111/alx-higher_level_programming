37e77e4a3dd59e5e194cb0aef15a4c09e9784663
Chinwe111 <chinwe.nworgu@gmail.com>
2023-05-09 16:09:06 -0700

    Print alphbt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 #3-print_alphabt.py
 
-for a in range(97, 123):
-    if chr(a) is not 'q' and chr(a) is not 'e':
-        print("{}".format(chr(a)), end="")
+"""Print the alphabet except q and e, not followed by a new line"""
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
