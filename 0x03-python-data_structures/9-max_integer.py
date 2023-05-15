552e2af90121d43314d04cb04f26a92864071300
Chinwe111 <chinwe.nworgu@gmail.com>
2023-05-15 15:02:52 -0700

    Max Value

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if (my_list is None):
+        return None
+    maxVal = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] > maxVal:
+            maxVal = my_list[i]
+    print("Max: {}".format(maxVal)
